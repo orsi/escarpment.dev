197375777f3b4aa017c879353fad8453cc9128be
orsi <jonathon.orsi@gmail.com>
2026-07-22 14:25:57 -0400

    feat: contact form

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,4 +1,27 @@
+"use client";
+import { SubmitEvent, useState } from "react";
+
+const CONTACT_URL = "https://escarpment-contact.jonathon-orsi.workers.dev";
+
 export default function ContactPage() {
+  const [message, setMessage] = useState<null | "success" | "error">(null);
+
+  const onSubmitForm = async (e: SubmitEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    const response = await fetch(CONTACT_URL, {
+      method: "POST",
+      body: formData,
+    });
+
+    if (!response.ok || response.status !== 200) {
+      setMessage("error");
+    } else {
+      setMessage("success");
+    }
+  };
+
   return (
     <>
       <section className="bg-white px-6 sm:px-8 lg:px-10 pt-32">
@@ -19,7 +42,7 @@ export default function ContactPage() {
 
       <section className="bg-white py-16 px-6 sm:px-8 lg:px-10">
         <div className="max-w-7xl mx-auto">
-          <form>
+          <form name="contact" onSubmit={onSubmitForm}>
             <div className="grid gap-8 md:grid-cols-2">
               <div className="flex flex-col gap-8">
                 <label className="text-sm font-bold">
@@ -27,6 +50,9 @@ export default function ContactPage() {
                   <input
                     className="text-base w-full px-3 py-2 rounded border"
                     placeholder="Your name"
+                    name="name"
+                    required
+                    maxLength={100}
                   />
                 </label>
                 <label className="text-sm font-bold">
@@ -35,6 +61,9 @@ export default function ContactPage() {
                     className="text-base w-full px-3 py-2 rounded border"
                     type="email"
                     placeholder="you@example.com"
+                    name="email"
+                    required
+                    maxLength={100}
                   />
                 </label>
                 <label className="text-sm font-bold">
@@ -42,6 +71,9 @@ export default function ContactPage() {
                   <input
                     className="text-base w-full px-3 py-2 rounded border"
                     placeholder="(555) 555-5555"
+                    name="phone"
+                    required
+                    maxLength={100}
                   />
                 </label>
               </div>
@@ -51,16 +83,32 @@ export default function ContactPage() {
                   <textarea
                     className="min-h-30 h-[calc(100%-1.2rem)] text-base w-full px-3 py-2 rounded border"
                     placeholder="Share a little about your goals, timeline, and preferred scope."
+                    name="message"
+                    maxLength={1000}
+                    required
                   />
                 </label>
               </div>
-              <div className="">
-                <button
-                  className="inline-flex rounded-full bg-primary-main px-6 py-1.5 font-bold text-white transition-transform duration-200 hover:-translate-y-0.5 cursor-pointer"
-                  type="submit"
-                >
-                  Submit
-                </button>
+              <div className="flex gap-8">
+                <div>
+                  <button
+                    className="inline-flex rounded-full bg-primary-main px-6 py-1.5 font-bold text-white transition-transform duration-200 hover:-translate-y-0.5 cursor-pointer"
+                    type="submit"
+                  >
+                    Submit
+                  </button>
+                </div>
+                {message === "success" ? (
+                  <span className="text-green-600">
+                    Success! We'll get back to you shortly.
+                  </span>
+                ) : message === "error" ? (
+                  <span className="text-red-600">
+                    Sorry, there was an error. Please try again.
+                  </span>
+                ) : (
+                  <></>
+                )}
               </div>
             </div>
           </form>
