2e740df3e32a1686c2e20221f06e262484230f90
orsi <jonathon.orsi@gmail.com>
2026-07-24 13:01:39 -0400

    feat: improve contact alert

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -4,21 +4,34 @@ import { SubmitEvent, useState } from "react";
 const CONTACT_URL = "https://escarpment-contact.jonathon-orsi.workers.dev";
 
 export default function ContactPage() {
-  const [message, setMessage] = useState<null | "success" | "error">(null);
+  const [status, setStatus] = useState<{
+    isLoading: boolean;
+    type: null | "success" | "error";
+  }>({ isLoading: false, type: null });
 
   const onSubmitForm = async (e: SubmitEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     const formData = new FormData(e.currentTarget);
-    const response = await fetch(CONTACT_URL, {
-      method: "POST",
-      body: formData,
-    });
+    setStatus({ isLoading: true, type: null });
 
-    if (!response.ok || response.status !== 200) {
-      setMessage("error");
-    } else {
-      setMessage("success");
+    try {
+      const response = await fetch(CONTACT_URL, {
+        method: "POST",
+        body: formData,
+      });
+
+      if (!response.ok || response.status !== 200) {
+        setStatus({ isLoading: false, type: "error" });
+      } else {
+        setStatus({ isLoading: false, type: "success" });
+      }
+    } catch (err) {
+      setStatus({ isLoading: false, type: "error" });
+    } finally {
+      setTimeout(() => {
+        setStatus({ isLoading: false, type: null });
+      }, 8000);
     }
   };
 
@@ -45,7 +58,7 @@ export default function ContactPage() {
           <form name="contact" onSubmit={onSubmitForm}>
             <div className="grid gap-8 md:grid-cols-2">
               <div className="flex flex-col gap-8">
-                <label className="text-sm font-bold">
+                <label className="text-sm font-bold" htmlFor="name">
                   Name
                   <input
                     className="text-base w-full px-3 py-2 rounded border"
@@ -55,7 +68,7 @@ export default function ContactPage() {
                     maxLength={100}
                   />
                 </label>
-                <label className="text-sm font-bold">
+                <label className="text-sm font-bold" htmlFor="email">
                   Email
                   <input
                     className="text-base w-full px-3 py-2 rounded border"
@@ -66,19 +79,23 @@ export default function ContactPage() {
                     maxLength={100}
                   />
                 </label>
-                <label className="text-sm font-bold">
+                <label className="text-sm font-bold" htmlFor="phone">
                   Phone
                   <input
                     className="text-base w-full px-3 py-2 rounded border"
                     placeholder="(555) 555-5555"
                     name="phone"
+                    type="tel"
                     required
                     maxLength={100}
                   />
                 </label>
               </div>
-              <div className="">
-                <label className="block h-full text-sm font-bold">
+              <div>
+                <label
+                  className="block h-full text-sm font-bold"
+                  htmlFor="message"
+                >
                   Project details
                   <textarea
                     className="min-h-30 h-[calc(100%-1.2rem)] text-base w-full px-3 py-2 rounded border"
@@ -89,23 +106,34 @@ export default function ContactPage() {
                   />
                 </label>
               </div>
-              <div className="flex gap-8">
-                <div>
-                  <button
-                    className="inline-flex rounded-full bg-primary-main px-6 py-1.5 font-bold text-white transition-transform duration-200 hover:-translate-y-0.5 cursor-pointer"
-                    type="submit"
+              <div>
+                <button
+                  className={`inline-flex rounded-full bg-primary-main px-6 py-1.5 font-bold text-white transition duration-200 hover:-translate-y-0.5 cursor-pointer ${status.isLoading ? "opacity-40 cursor-default!" : ""}`}
+                  disabled={status.isLoading}
+                  type="submit"
+                >
+                  Submit
+                </button>
+              </div>
+              <div className="md:row-3">
+                {status.type === "success" ? (
+                  <div
+                    className="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative"
+                    role="alert"
                   >
-                    Submit
-                  </button>
-                </div>
-                {message === "success" ? (
-                  <span className="text-green-600">
-                    Success! We'll get back to you shortly.
-                  </span>
-                ) : message === "error" ? (
-                  <span className="text-red-600">
-                    Sorry, there was an error. Please try again.
-                  </span>
+                    <span className="block sm:inline">
+                      Success! We'll get back to you shortly.
+                    </span>
+                  </div>
+                ) : status.type === "error" ? (
+                  <div
+                    className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+                    role="alert"
+                  >
+                    <span className="block sm:inline">
+                      Sorry, there was an error. Please try again.
+                    </span>
+                  </div>
                 ) : (
                   <></>
                 )}
